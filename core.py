6ca9e10566f809ade768c8be21425607f349d843
Vive-bit <77213543+Vive-bit@users.noreply.github.com>
2024-04-29 20:01:43 +0200

    add

--- core.py
+++ core.py
@@ -1,3 +1,6 @@
 import src.classes.logger.logging
 from src.classes.logger.logging import LoggingManager as LMCG
 LMCG().log(type="global").info("[SETUP] Program initializing...")
+
+
+LMCG().log(type="global").info("zeile 3 code")
