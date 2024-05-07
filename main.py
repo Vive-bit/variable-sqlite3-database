15ada6f697f58b3f2b2734a38247fd423086e4f8
GeneralxJosef <168465856+GeneralxJosef@users.noreply.github.com>
2024-05-07 20:26:24 +0200

    Semi Finished Homescreen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import src.game.homescreen as homescreen
-global mouse, statusStart, statusSetting
+global mouse, statusStart, statusSetting, statusQuit
 
 pygame.init()
 
@@ -11,6 +11,9 @@ surface = window
 
 pygame.display.set_caption("Halle7")
 
+# Define Var's
+statusQuit = 0
+
 running = True
 while running:
     mouse = pygame.mouse.get_pos()
@@ -19,28 +22,38 @@ while running:
             running = False
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
+
+        elif homescreen.beendenSureYes(surface).collidepoint(mouse) and event.type == pygame.MOUSEBUTTONDOWN:
+            running = False
+
+        elif homescreen.beendenSureNo(surface).collidepoint(mouse) and event.type == pygame.MOUSEBUTTONDOWN:
+            statusQuit = 0 # Disable "BeendenSure" Window
+
         elif homescreen.startIMG(surface).collidepoint(mouse):
-            print("Start")
+            pass
+
         elif homescreen.einstellungIMG(surface).collidepoint(mouse):
-            print("Einstellung")
-        elif homescreen.beendenIMG(surface).collidepoint(mouse) and event.type == pygame.KEYDOWN and\
-                event.key == pygame.MOUSEBUTTONDOWN:
-            homescreen.beendensure(surface)
+            pass
 
-        elif homescreen.beendenIMG(surface).collidepoint(mouse):
-            print("Beenden")
-        elif event.type == pygame.KEYDOWN and event.key == pygame.MOUSEBUTTONDOWN:
-            print("allah")
+        elif homescreen.beendenIMG(surface).collidepoint(mouse) and event.type == pygame.MOUSEBUTTONDOWN:
+            statusQuit = 1 # Enable "BeendenSure" Window
 
-    # Standard window background with 60FPS setting
 
+
+
+
+    # Standard window background with 60 FPS setting
     clock = pygame.time.Clock()         # FPS setting = 60FPS
     delta_time = clock.tick(60) / 1000  # FPS setting = 60 FPS
     window.fill((255, 255, 255))        # Filling background to white
 
     # Update
 
-    homescreen.update(surface)          # Homescreen design updating
+    # Checks if "BeendenSure" Window should appear
+    if statusQuit == 0:
+        homescreen.updateN(surface)     # Homescreen design updating when "No"
+    else:
+        homescreen.updateQ(surface)     # Homescreen design updating when "Yes"
 
     pygame.display.update()
 
--- src/game/homescreen.py
+++ src/game/homescreen.py
@@ -1,13 +1,14 @@
-import pygame
 
-def backgroundIMG(surface):
+import pygame
+def backgroundIMG(surface): # Background
     backgroundimgraw = pygame.image.load("pictures/PalfingerHintergrund.png")
     backgroundimg = pygame.Surface.convert_alpha(backgroundimgraw)
     sizebackground = pygame.transform.scale(backgroundimg, (1920, 1080))
     x = surface.blit(sizebackground, (0, 0))
 
     return x
-def startIMG(surface):
+
+def startIMG(surface): # Start Button
     startimgraw = pygame.image.load("pictures/Start.png")       # Load an image
     startimg = pygame.Surface.convert_alpha(startimgraw)        # Optimize image
     sizestart = pygame.transform.scale(startimg, (500, 100))    # Set size of image
@@ -15,7 +16,7 @@ def startIMG(surface):
 
     return x
 
-def einstellungIMG(surface):
+def einstellungIMG(surface): # Settings Button
     settingimgraw = pygame.image.load("pictures/Einstellungen.png")     # Load an image
     settingimg = pygame.Surface.convert_alpha(settingimgraw)            # Optimize image
     sizesetting = pygame.transform.scale(settingimg, (500, 100))        # Set size of image
@@ -23,25 +24,44 @@ def einstellungIMG(surface):
 
     return x
 
-
-def beendenIMG(surface):
+def beendenIMG(surface): # Quit Button
     beendenimgraw = pygame.image.load("pictures/Beenden.png")  # Load an image
     beendenimg = pygame.Surface.convert_alpha(beendenimgraw)  # Optimize image
     sizebeenden = pygame.transform.scale(beendenimg, (500, 100))  # Set size of image
     x = surface.blit(sizebeenden, (1920 / 2 - 250, 400))
 
     return x
-def beendensure(surface):
-    pygame.draw.rect(surface, "gray", [(1920/2-250, 1080/2-200), (500, 400)])
+def beendenSureRect(surface):  # "Sure you want to quit" Buttons and Window
+    pygame.draw.rect(surface, "gray", [(1920/2-250, 1080/2-200), (500, 400)])   # Draw the quit box
 
+def beendenSureYes(surface):
+    beendensureyesimgraw = pygame.image.load("pictures/BeendenText.png")  # Load an image
+    beendensureyesimg = pygame.Surface.convert_alpha(beendensureyesimgraw)  # Optimize image
+    sizebeendensureyes = pygame.transform.scale(beendensureyesimg, (100, 50))  # Set size of image
+    x = surface.blit(sizebeendensureyes, (1920 / 2 - 200, 1080 / 2 + 100))
 
+    return x
 
-def update(surface):
+def beendenSureNo(surface):
+    beendensurenoimgraw = pygame.image.load("pictures/AbbrechenText.png")  # Load an image
+    beendensurenoimg = pygame.Surface.convert_alpha(beendensurenoimgraw)  # Optimize image
+    sizebeendensureno = pygame.transform.scale(beendensurenoimg, (100, 50))  # Set size of image
+    x = surface.blit(sizebeendensureno, (1920 / 2 + 100, 1080 / 2 + 100))
+
+    return x
+def updateN(surface):
     backgroundIMG(surface)
     startIMG(surface)
     einstellungIMG(surface)
     beendenIMG(surface)
-    #beendensure(surface)
+
+def updateQ(surface):
+    backgroundIMG(surface)
+    beendenSureRect(surface)
+    beendenSureYes(surface)
+    beendenSureNo(surface)
+
+
 
 
 
