c9db5a9835e9725d207a47a2de10d523f3788f5c
GeneralxJosef <168465856+GeneralxJosef@users.noreply.github.com>
2024-05-11 21:13:06 +0200

    Homescreen V1.0

--- src/game/homescreen.py
+++ src/game/homescreen.py
@@ -32,9 +32,21 @@ def beendenIMG(surface): # Quit Button
 
     return x
 def beendenSureRect(surface):  # "Sure you want to quit" Buttons and Window
-    pygame.draw.rect(surface, "gray", [(1920/2-250, 1080/2-200), (500, 400)])   # Draw the quit box
+    #pygame.draw.rect(surface, "yellow", [(1920/2-250, 1080/2-200), (500, 400)])   # Draw the quit box
+    beendensureboximgraw = pygame.image.load("pictures/BeendenSureBox.png") # Load an image
+    beendensureboximg = pygame.Surface.convert_alpha(beendensureboximgraw) # Optimize image
+    sizebeendensurebox = pygame.transform.scale(beendensureboximg, (500, 400)) # Set size of image
+    x = surface.blit(sizebeendensurebox, (1920 / 2 - 250, 1080 / 2 - 200))
+    pygame.draw.rect(surface, "red", [(1920 / 2 - 250, 1080 / 2 - 200), (500, 400)], 4)  # Draw the quit box outline
+    pygame.draw.rect(surface, "black", [(1920 / 2 - 230, 1080 / 2 + 75), (160, 100)])  # Draw the quit box buttons box 1
+    pygame.draw.rect(surface, "red", [(1920 / 2 - 230, 1080 / 2 + 75), (160, 100)], 4)  # Draw the quit box buttons box outlines 1
+    pygame.draw.rect(surface, "black", [(1920 / 2 + 70, 1080 / 2 + 75), (160, 100)])  # Draw the quit box buttons box 2
+    pygame.draw.rect(surface, "red", [(1920 / 2 + 70, 1080 / 2 + 75), (160, 100)], 4)  # Draw the quit box buttons box outlines 2
 
-def beendenSureYes(surface):
+    return x
+
+
+def beendenSureYes(surface): # Quit sure button
     beendensureyesimgraw = pygame.image.load("pictures/BeendenText.png")  # Load an image
     beendensureyesimg = pygame.Surface.convert_alpha(beendensureyesimgraw)  # Optimize image
     sizebeendensureyes = pygame.transform.scale(beendensureyesimg, (100, 50))  # Set size of image
@@ -42,24 +54,34 @@ def beendenSureYes(surface):
 
     return x
 
-def beendenSureNo(surface):
+def beendenSureNo(surface): # Quit not button
     beendensurenoimgraw = pygame.image.load("pictures/AbbrechenText.png")  # Load an image
     beendensurenoimg = pygame.Surface.convert_alpha(beendensurenoimgraw)  # Optimize image
     sizebeendensureno = pygame.transform.scale(beendensurenoimg, (100, 50))  # Set size of image
     x = surface.blit(sizebeendensureno, (1920 / 2 + 100, 1080 / 2 + 100))
 
     return x
-def updateN(surface):
+
+def beendenSureText(surface): # Quit sure text
+    beendensuretextimgraw = pygame.image.load("pictures/BeendenSureText.png")       # Load an image
+    beendensuretextimg = pygame.Surface.convert_alpha(beendensuretextimgraw)        # Optimize image
+    sizebeendensuretext = pygame.transform.scale(beendensuretextimg, (500, 250))    # Set size of image
+    x = surface.blit(sizebeendensuretext, (1920/2-250, 1080/2-200))
+
+    return x
+
+def updateN(surface): # Update when not "BeendenSure"
     backgroundIMG(surface)
     startIMG(surface)
     einstellungIMG(surface)
     beendenIMG(surface)
 
-def updateQ(surface):
+def updateQ(surface): # Update when "BeendenSure"
     backgroundIMG(surface)
     beendenSureRect(surface)
     beendenSureYes(surface)
     beendenSureNo(surface)
+    beendenSureText(surface)
 
 
 
